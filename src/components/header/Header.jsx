33f4e51556f5bb418f8b80788add0aed7c76e8e6
Claudiu Petre <claudiu.gpetre@gmail.com>
2023-06-27 08:15:40 +0100

    header styling

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,11 +1,29 @@
 import React from 'react'
 import './header.css'
+import CTA from './CTA'
+import Me from '../../pics/Me.jpeg'
+import HeaderSocials from './HeaderSocials'
 
 const Header = () => {
   return (
-    <div>
-      Header
+  <header>
+    <div className='container header_container'>
+      <h5>Hello I am</h5>
+      <h1>Claudiu Petre</h1>
+      <h5 className='text-light'>Fullstack Developer</h5>
+      <CTA />
+      <HeaderSocials />
+
+      <div className='me'>
+        <img src={Me} alt='me'/>
+      </div>
+
+      <a href='#contact' className='scroll__down'>Scroll Down</a>
+
     </div>
+
+   </header>
+
   )
 }
 
