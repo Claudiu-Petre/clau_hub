8e27805e272f25fdf1c1be337543b5e14822869f
Claudiu Petre <claudiu.gpetre@gmail.com>
2023-07-01 20:07:01 +0100

    portfolio final

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,55 +1,55 @@
 import React from 'react'
 import './portfolio.css'
-import IMG1 from '../../pics/portfolio_1.webp'
-import IMG2 from '../../pics/portfolio_1.webp'
-import IMG3 from '../../pics/portfolio_1.webp'
-import IMG4 from '../../pics/portfolio_1.webp'
-import IMG5 from '../../pics/portfolio_1.webp'
-import IMG6 from '../../pics/portfolio_1.webp'
+import IMG1 from '../../pics/dashboard.webp'
+import IMG2 from '../../pics/meteorologist.webp'
+import IMG3 from '../../pics/mapper.webp'
+import IMG4 from '../../pics/dosher.webp'
+import IMG5 from '../../pics/cruiseship.webp'
+import IMG6 from '../../pics/the_bookist.webp'
 
 
 const data = [
   {
     id: 1,
     image: IMG1,
-    title: 'Orion UI kit - Charts templates & infographics in Figma',
-    github: 'http://github.com',
-    demo: 'https://dribbble.com/shots/14796898-Orion-UI-kit-Charts-templates-infographics-in-Figma'
-  },
-  {
-    id: 1,
-    image: IMG2,
-    title: 'Title two',
+    title: 'Dashboard, Python',
     github: 'http://github.com',
     demo: 'https://dribbble.com/shots/14796898-Orion-UI-kit-Charts-templates-infographics-in-Figma'
   },
   {
     id: 2,
-    image: IMG3,
-    title: 'Title three',
+    image: IMG2,
+    title: 'Meteorologist',
     github: 'http://github.com',
-    demo: 'https://dribbble.com/shots/14796898-Orion-UI-kit-Charts-templates-infographics-in-Figma'
+    demo: 'https://dribbble.com/shots/21606628-Weather-UI-for-Android'
   },
   {
     id: 3,
+    image: IMG3,
+    title: 'The Mapper',
+    github: 'http://github.com',
+    demo: 'https://dribbble.com/shots/18935350-Milkinside-Globe'
+  },
+  {
+    id: 4,
     image: IMG4,
-    title: 'Title four',
+    title: 'Do$her',
     github: 'http://github.com',
-    demo: 'hhttps://dribbble.com/shots/14796898-Orion-UI-kit-Charts-templates-infographics-in-Figma'
+    demo: 'https://dribbble.com/search/pounds'
   },
   {
-    id: 1,
+    id: 5,
     image: IMG5,
-    title: 'Title five',
+    title: 'Cruise Ship',
     github: 'http://github.com',
-    demo: 'https://dribbble.com/shots/14796898-Orion-UI-kit-Charts-templates-infographics-in-Figma'
+    demo: 'https://dribbble.com/shots/4395535-BOS-Corporate-Event-2'
   },
   {
-    id: 1,
+    id: 6,
     image: IMG6,
-    title: 'Title six',
+    title: 'The Bookist',
     github: 'http://github.com',
-    demo: 'https://dribbble.com/shots/14796898-Orion-UI-kit-Charts-templates-infographics-in-Figma'
+    demo: 'https://dribbble.com/shots/20454786-E-Book-Dashboard'
   }
 ]
 
