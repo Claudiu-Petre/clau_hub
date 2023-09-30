79e905097e3a86dcd03241320f908cf282ff21a5
Claudiu Petre <claudiu.gpetre@gmail.com>
2023-09-30 15:41:39 +0100

    colors

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -6,12 +6,14 @@ import {BiBook} from 'react-icons/bi'
 import {RiServiceLine} from 'react-icons/ri'
 import {BiMessageRoundedDetail} from 'react-icons/bi'
 import { useState } from 'react'
+// import {FiNavigation2} from 'react-icons/fi'
 
 const Nav = () => {
-  const [activeNav, setActiveNav] = useState('#home')
+  const [activeNav, setActiveNav] = useState('#header')
   return (
     <nav>
       <a href='#home' onClick={() =>setActiveNav('#home')} className={activeNav === '#home' ? 'active' :'' }><AiOutlineHome/></a>
+      {/* <a href='#header' onClick={() =>setActiveNav('#header')} className={activeNav === '#header' ? 'active' :'' }><FiNavigation2/></a> */}
       <a href='#about' onClick={() =>setActiveNav('#about')} className={activeNav === '#about' ? 'active' :'' }><AiOutlineUser/></a>
       <a href='#experience' onClick={() =>setActiveNav('#experience')} className={activeNav === '#experience' ? 'active' :'' }><BiBook/></a>
       <a href='#portfolio' onClick={() =>setActiveNav('#portfolio')} className={activeNav === '#portfolio' ? 'active' :'' }><RiServiceLine/></a>
