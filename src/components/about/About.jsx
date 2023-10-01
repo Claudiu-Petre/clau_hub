8633d33fd90f8e980bd2b9aa47373fc4a84b9e73
Claudiu Petre <claudiu.gpetre@gmail.com>
2023-10-01 21:29:02 +0100

    colours and pics

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import './about.css'
-import Me from '../../pics/IMG_2320.jpg'
+import Me from '../../pics/whitewall.png'
 import {FaAward} from 'react-icons/fa'
 import {FiUsers} from 'react-icons/fi'
 import {VscFolderLibrary} from 'react-icons/vsc'
