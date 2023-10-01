310d0ea2a99524ca8762ed389f5097e4e7fb7ea8
Claudiu Petre <claudiu.gpetre@gmail.com>
2023-10-01 22:08:30 +0100

    blur

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -9,7 +9,7 @@ import { useState } from 'react'
 // import {FiNavigation2} from 'react-icons/fi'
 
 const Nav = () => {
-  const [activeNav, setActiveNav] = useState('#header')
+  const [activeNav, setActiveNav] = useState('#home')
   return (
     <nav>
       <a href='#home' onClick={() =>setActiveNav('#home')} className={activeNav === '#home' ? 'active' :'' }><AiOutlineHome/></a>
